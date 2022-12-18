4d0842ccb26cab969bb56ee3afa1476a954bf4ed
nagamoto <koutaro.n.1056@gmail.com>
2022-12-18 14:00:40 +0900

    initial commit

---
+++ src/test/java/com/rasetech/java9/Java9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasetech.java9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
