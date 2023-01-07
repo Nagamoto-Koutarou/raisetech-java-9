d70f89f5de270ebb1a0645e40c25d50ab8d5ae74
nagamoto <koutaro.n.1056@gmail.com>
2023-01-07 23:25:04 +0900

    データベースの変更と追加処理

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -1,13 +1,34 @@
- DROP TABLE IF EXISTS names;
+ DROP TABLE IF EXISTS coffees;
 
- CREATE TABLE names (
+ CREATE TABLE coffees (
     id int unsigned AUTO_INCREMENT,
-    name VARCHAR(20) NOT NULL,
+    productName VARCHAR(20) NOT NULL,
+    countryOfOrigin VARCHAR(20) NOT NULL,
+    degreeOfRoasting VARCHAR(20) NOT NULL,
+    aroma int,
+    acidity int,
+    bitterTaste int,
+    sweetTaste int,
+    rich int,
+    100gPrice decimal(7, 2),
+    200gPrice decimal(7, 2),
     PRIMARY KEY (id)
   );
 
-  INSERT INTO names (name) VALUES ("nagamoto");
-  INSERT INTO names (name) VALUES ("tanaka");
-  INSERT INTO names (name) VALUES ("sato");
-  INSERT INTO names (name) VALUES ("murakami");
-  INSERT INTO names (name) VALUES ("yoshida");
+ INSERT INTO coffees (productName, countryOfOrigin, degreeOfRoasting, aroma, acidity, bitterTaste, sweetTaste, rich, 100gPrice, 200gPrice) VALUES
+ ("ブルーマウンテン", "ジャマイカ", "浅煎り", 3, 2, 1, 2, 3, 2850, 5650),
+ ("ハワイ　エクストラファンシー", "アメリカ", "やや浅煎り", 2, 1, 2, 2, 3, 1700, 3350),
+ ("モカ　マタリ", "イエメン", "やや浅煎り", 3, 1, 1, 2, 3, 950, 1850),
+ ("クリスタルマウンテン", "キューバ", "浅煎り", 3, 2, 1, 2, 2, 1000, 1950),
+ ("ペルー・チャンチャマイヨ", "ペルー", "浅煎り", 2, 1, 2, 2, 3, 700, 1350),
+ ("ストラウエシ・トラジャ", "インドネシア", "深煎り", 2, 0, 3, 2, 3, 800, 1550),
+ ("グァテマラ SHB", "グァテマラ", "深煎り", 2, 1, 3, 2, 3, 700, 1350),
+ ("ブラジル・サントス NO.2", "ブラジル", "強・深煎り"2, 0, 2, 2, 3, 700, 1350),
+ ("キリマンジャロAA", "タンザニア", "やや深煎り", 2, 2, 1, 1, 2, 700, 1350),
+ ("マンデリン　グレードⅠ", "インドネシア", "やや深煎り", 2, 0, 2, 2, 3, 700, 1350),
+ ("コロンビア・スプレモ", "コロンビア", "強・深煎り", 2, 0, 2, 3, 3, 700, 1350),
+ ("マラウィゲイシャ AA", "マラウィ", "やや深煎り", 3, 1, 2, 2, 3, 850, 1650),
+ ("ケニア AA", "ケニア", "やや深煎り", 2, 2, 1, 2, 2, 700, 1350),
+ ("デカフェ（カフェインレス）", "コロンビア", "やや深煎り", 2, 0, 2, 2, 2, 700, 1350),
+ ("ガラパゴス　ブルボン", "エクアドル", "やや深煎り", 2, 1, 2, 2, 2, 950, 1850),
+ ("ヒマラヤン　アラビカ", "ネパール", "やや深煎り", 2, 1, 2, 2, 2, 950, 1850);
